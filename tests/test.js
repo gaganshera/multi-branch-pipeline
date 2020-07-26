f1067e14a898e544a1985ee6199c3c240b35f15f
ss <ss@ss.com>
2020-07-27 04:16:56 +0530

    UPT: test cases

--- tests/test.js
+++ tests/test.js
@@ -4,8 +4,20 @@ var app = require('../app');
 // var app = express();
 describe(`GET /`, function () {
     it(`respond with hello world`, function(done) {
+        request(app).get(`/users`).expect(`respond with a resource`, done);
+    });
+});
+
+describe(`Other cases`, function () {
+    it(`Return 200`, function (done) {
         //navigate to root and check the the response is "hello world"
         // console.log(request(app).get(`/users`))
-        request(app).get(`/users`).expect(`respond with a resource`, done);
- });
-});
+        request(app).get(`/`).expect(200, done);
+    });
+
+    it(`Should return 404`, function (done) {
+        //navigate to root and check the the response is "hello world"
+        // console.log(request(app).get(`/users`))
+        request(app).get(`/sakldjhgcvf`).expect(404, done);
+    });
+})
