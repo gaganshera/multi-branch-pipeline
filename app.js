ad663b0de5b34f7088544125d40c4f4c72f3a23a
ss <ss@ss.com>
2020-08-08 20:55:23 +0530

    TEMP: test sonar

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+let test = '';
 var app = express();
 
 // view engine setup
