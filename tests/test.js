af67473c196456ac5e6aa82a51295efe55660916
Gaganjot Singh <gaganjot.singh02@nagarro.com>
2021-07-29 04:11:52 +0530

    Formatting changes

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,17 @@
 let request = require(`supertest`);
 let app = require('../app');
 describe(`GET /`, function () {
-    it(`respond with hello world`, function(done) {
-        request(app).get(`/users`).expect(`respond with a resource`, done);
-    });
+  it(`respond with hello world`, function (done) {
+    request(app).get(`/users`).expect(`respond with a resource`, done);
+  });
 });
 
 describe(`Other cases`, function () {
-    it(`Return 200`, function (done) {
-        //navigate to root and check the the response is "hello world"
-        // console.log(request(app).get(`/users`))
-        request(app).get(`/`).expect(200, done);
-    });
+  it(`Return 200`, function (done) {
+    request(app).get(`/`).expect(200, done);
+  });
 
-    it(`Should return 404`, function (done) {
-        //navigate to root and check the the response is "hello world"
-        // console.log(request(app).get(`/users`))
-        request(app).get(`/sakldjhgcvf`).expect(404, done);
-    });
-})
+  it(`Should return 404`, function (done) {
+    request(app).get(`/sakldjhgcvf`).expect(404, done);
+  });
+});
